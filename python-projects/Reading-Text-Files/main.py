4e0d7f92ab1885fffcb44a9682af9b7a6ce1dc67
rocket <37631123+gamernewone@users.noreply.github.com>
2022-05-28 22:48:04 +0000

    reading file function fixed

--- python-projects/Reading-Text-Files/main.py
+++ python-projects/Reading-Text-Files/main.py
@@ -1,16 +1,27 @@
 # Read text from a file, and count the occurence of words in that text
 # Example:
-# count_words("The cake is done. It is a big cake!") 
+# count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    # [assignment] Add your code here
+    handle = open(filename)
+
+    return handle
 
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    counts = dict()
+    for line in text:
+        words = line.split()
 
-    return {"as": 10, "would": 20}
+        for word in words:
+            counts[word] = counts.get(word, 0) + 1
+
+    return counts
+
+
+answer = count_words()
+print(answer)
